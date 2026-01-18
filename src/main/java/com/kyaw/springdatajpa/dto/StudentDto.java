37344dab3d8fa6a88cb3ffe6ba5845367199686c
Kyaw Hlaing Soe <kyawhlaingsoe572@gmail.com>
2026-01-18 21:44:06 +0630

    feat: enhance validation messages in StudentDto for first and last name fields

--- src/main/java/com/kyaw/springdatajpa/dto/StudentDto.java
+++ src/main/java/com/kyaw/springdatajpa/dto/StudentDto.java
@@ -4,9 +4,9 @@ package com.kyaw.springdatajpa.dto;
 import jakarta.validation.constraints.NotEmpty;
 
 public record StudentDto(
-    @NotEmpty
+    @NotEmpty(message = "first name must not be empty")
     String firstName,
-    @NotEmpty
+    @NotEmpty(message = "last name must not be empty")
     String lastName,
     @NotEmpty
     String email,
